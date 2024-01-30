f3d2f8cb48749dcc2a32a3ec61f8fa49ec2532ba
DeokWon Kim <khjs7878@naver.com>
2024-01-30 22:27:15 +0900

    scrollanimation추가 및 our_history.html추가

--- main.js
+++ main.js
@@ -1,41 +1,562 @@
 let mainText = document.querySelector(".first_scroll")
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 386) {
+        mainText.style.animation = "scroll1 2s ease-out forwards"
+    } else {
+        mainText.style.animation = "scrollback 2s ease-out forwards"
+    }
+})
+
 let mainText2 = document.querySelector(".numbers")
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 1616) {
+        mainText2.style.animation = "scroll2 2s ease-in-out forwards"
+    } else {
+        mainText2.style.animation = "scrollback2 2s ease-in-out forwards"
+    }
+})
+
 let mainText3 = document.querySelector(".service_title")
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 1616) {
+        mainText3.style.animation = "scroll3 2s ease-in-out forwards"
+    } else {
+        mainText3.style.animation = "scrollback3 2s ease-in-out forwards"
+    }
+})
+
 let mainText4 = document.querySelector(".buildings")
 
-window.addEventListener("scroll",function(){
+window.addEventListener("scroll", function () {
     let value = window.scrollY;
-    console.log("scrollY",value);
+    console.log("scrollY", value);
 
-    if(value > 386){
-        mainText.style.animation="scroll1 2s ease-out forwards"
+    if (value > 1616) {
+        mainText4.style.animation = "scroll4 2s ease-in-out forwards"
+    } else {
+        mainText4.style.animation = "scrollback4 2s ease-in-out forwards"
+    }
+})
+
+let mainImg = document.querySelector(".uvu")
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 3090) {
+        mainImg.style.animation = "scroll5 2s ease-in-out forwards"
+    } else {
+        mainImg.style.animation = "scrollback5 2s ease-in-out forwards"
+    }
+})
+
+let Logo = document.querySelector(".logo")
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 3090) {
+        Logo.style.animation = "scroll6 2s ease-in-out forwards"
+    } else {
+        Logo.style.animation = "scrollback6 2s ease-in-out forwards"
+    }
+})
+
+let logoback = document.querySelector(".logo_background")
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 3090) {
+        logoback.style.animation = "scroll7 2s ease-in-out forwards"
+    } else {
+        logoback.style.animation = "scrollback7 2s ease-in-out forwards"
+    }
+})
+
+let mainImg2 = document.querySelector(".thomas")
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 3090) {
+        mainImg2.style.animation = "scroll8 2s ease-in-out forwards"
+    } else {
+        mainImg2.style.animation = "scrollback8 2s ease-in-out forwards"
+    }
+})
+
+let logoback2 = document.querySelector(".logo_background2")
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 3090) {
+        logoback2.style.animation = "scroll8 2s ease-in-out forwards"
+    } else {
+        logoback2.style.animation = "scrollback8 2s ease-in-out forwards"
+    }
+})
+
+let Logo2 = document.querySelector(".logo2")
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 3090) {
+        Logo2.style.animation = "scroll9 2s ease-in-out forwards"
+    } else {
+        Logo2.style.animation = "scrollback9 2s ease-in-out forwards"
+    }
+})
+
+let mainImg3 = document.querySelector(".usu")
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 3654) {
+        mainImg3.style.animation = "scroll10 2s ease-in-out forwards"
+    } else {
+        mainImg3.style.animation = "scrollback10 2s ease-in-out forwards"
+    }
+})
+
+let logoback3 = document.querySelector(".logo_background3")
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 3654) {
+        logoback3.style.animation = "scroll10 2s ease-in-out forwards"
+    } else {
+        logoback3.style.animation = "scrollback10 2s ease-in-out forwards"
+    }
+})
+
+let Logo3 = document.querySelector(".logo3")
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 3654) {
+        Logo3.style.animation = "scroll11 2s ease-in-out forwards"
+    } else {
+        Logo3.style.animation = "scrollback11 2s ease-in-out forwards"
+    }
+})
+
+let mainImg4 = document.querySelector(".num45")
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 3654) {
+        mainImg4.style.animation = "numberscroll 2s ease-in-out forwards"
+    } else {
+        mainImg4.style.animation = "numberscrollback 2s ease-in-out forwards"
+    }
+})
+
+let number = document.querySelector('.usu_desc')
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 3654) {
+        number.style.animation = "numberscroll 2s ease-in-out forwards"
+    } else {
+        number.style.animation = "numberscrollback 2s ease-in-out forwards"
+    }
+})
+
+/* uvu2 클래스애니메이션*/
+let uvu2 = document.querySelector('.uvu2')
+
+window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 4308) {
+        uvu2.style.animation = 'uvu2 2s ease-in-out forwards'
     }
 
     else {
-        mainText.style.animation="scrollback 2s ease-out forwards"
+        uvu2.style.animation = 'uvu2back 2s ease-in-out forwards'
     }
+})
 
-    if(value > 1616){
-        mainText2.style.animation="scroll2 2s ease-in-out forwards"
-    }
+/* logo_background4 클래스애니메이션*/
+let logo_background4 = document.querySelector('.logo_background4')
 
-    else {
-        mainText2.style.animation="scrollback2 2s ease-in-out forwards"
-    }
+window.addEventListener("scroll", function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
 
-    if(value > 1616){
-        mainText3.style.animation="scroll3 2s ease-in-out forwards"
-    }
-
-    else {
-        mainText3.style.animation="scrollback3 2s ease-in-out forwards"
-    }
-
-    if(value > 1616){
-        mainText4.style.animation="scroll4 2s ease-in-out forwards"
+    if (value > 4308) {
+        logo_background4.style.animation = 'logobackground4 2s ease-in-out forwards' 
     }
 
     else {
-        mainText4.style.animation="scrollback4 2s ease-in-out forwards"
+        logo_background4.style.animation = 'logobackground4back 2s ease-in-out forwards'
+    }
+})
+
+/* logo4 클래스애니메이션 */ 
+let logo4 = document.querySelector('.logo4')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 4308) {
+        logo4.style.animation = 'logo4 2s ease-in-out forwards' 
+    }
+
+    else {
+        logo4.style.animation = 'logo4back 2s ease-in-out forwards'
+    }
+})
+
+/* usu2 클래스애니메이션 */ 
+let usu2 = document.querySelector('.usu2')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 4308) {
+        usu2.style.animation = 'usu2 2s ease-in-out forwards' 
+    }
+
+    else {
+        usu2.style.animation = 'usu2back 2s ease-in-out forwards'
+    }
+})
+
+/* logo_background5 클래스애니메이션 */ 
+let logo_background5 = document.querySelector('.logo_background5')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 4308) {
+        logo_background5.style.animation = 'logo_background5 2s ease-in-out forwards' 
+    }
+
+    else {
+        logo_background5.style.animation = 'logo_background5back 2s ease-in-out forwards'
+    }
+})
+
+/* logo5 클래스애니메이션 */ 
+let logo5 = document.querySelector('.logo5')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 4308) {
+        logo5.style.animation = 'logo5 2s ease-in-out forwards' 
+    }
+
+    else {
+        logo5.style.animation = 'logo5back 2s ease-in-out forwards'
+    }
+})
+
+/* sugar 클래스애니메이션 */ 
+let sugar = document.querySelector('.sugar')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 5400) {
+        sugar.style.animation = 'sugar 2s ease-in-out forwards' 
+    }
+
+    else {
+        sugar.style.animation = 'sugarback 2s ease-in-out forwards'
+    }
+})
+
+/* logo_background6 클래스애니메이션 */ 
+let logo_background6 = document.querySelector('.logo_background6')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 5400) {
+        logo_background6.style.animation = 'logo_background6 2s ease-in-out forwards' 
+    }
+
+    else {
+        logo_background6.style.animation = 'logo_background6back 2s ease-in-out forwards'
+    }
+})
+
+/* logo6 클래스애니메이션 */ 
+let logo6 = document.querySelector('.logo6')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 5400) {
+        logo6.style.animation = 'logo6 2s ease-in-out forwards' 
+    }
+
+    else {
+        logo6.style.animation = 'logo6back 2s ease-in-out forwards'
+    }
+})
+
+/* uta 클래스애니메이션 */ 
+let uta = document.querySelector('.uta')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 5400) {
+        uta.style.animation = 'uta 2s ease-in-out forwards' 
+    }
+
+    else {
+        uta.style.animation = 'utaback 2s ease-in-out forwards'
+    }
+})
+
+/* logo_background7 클래스애니메이션 */ 
+let logo_background7 = document.querySelector('.logo_background7')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 5400) {
+        logo_background7.style.animation = 'uta 2s ease-in-out forwards' 
+    }
+
+    else {
+        logo_background7.style.animation = 'utaback 2s ease-in-out forwards'
+    }
+})
+
+/* logo7 클래스애니메이션 */ 
+let logo7 = document.querySelector('.logo7')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 5400) {
+        logo7.style.animation = 'logo7 2s ease-in-out forwards' 
+    }
+
+    else {
+        logo7.style.animation = 'logo7back 2s ease-in-out forwards'
+    }
+})
+
+/* unified 클래스애니메이션 */ 
+let unified = document.querySelector('.unified')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 5940) {
+        unified.style.animation = 'unified 2s ease-in-out forwards' 
+    }
+
+    else {
+        unified.style.animation = 'unifiedback 2s ease-in-out forwards'
+    }
+})
+
+/* logo_background8 클래스애니메이션 */ 
+let logo_background8 = document.querySelector('.logo_background8')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 5940) {
+        logo_background8.style.animation = 'logo_background8 2s ease-in-out forwards' 
+    }
+
+    else {
+        logo_background8.style.animation = 'logo_background8back 2s ease-in-out forwards'
+    }
+})
+
+/* logo8 클래스애니메이션 */ 
+let logo8 = document.querySelector('.logo8')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 5940) {
+        logo8.style.animation = 'logo8 2s ease-in-out forwards' 
+    }
+
+    else {
+        logo8.style.animation = 'logo8back 2s ease-in-out forwards'
+    }
+})
+
+/* sutton 클래스애니메이션 */ 
+let sutton = document.querySelector('.sutton')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 5940) {
+        sutton.style.animation = 'sutton 2s ease-in-out forwards' 
+    }
+
+    else {
+        sutton.style.animation = 'suttonback 2s ease-in-out forwards'
+    }
+})
+
+/* logo_background9 클래스애니메이션 */ 
+let logo_background9 = document.querySelector('.logo_background9')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 5940) {
+        logo_background9.style.animation = 'sutton 2s ease-in-out forwards' 
+    }
+
+    else {
+        logo_background9.style.animation = 'suttonback 2s ease-in-out forwards'
+    }
+})
+
+/* logo9 클래스애니메이션 */ 
+let logo9 = document.querySelector('.logo9')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 5940) {
+        logo9.style.animation = 'logo9 2s ease-in-out forwards' 
+    }
+
+    else {
+        logo9.style.animation = 'logo9 2s ease-in-out forwards'
+    }
+})
+
+/* k 클래스애니메이션 */ 
+let k = document.querySelector('.k')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 6713) {
+        k.style.animation = 'k 2s ease-in-out forwards' 
+    }
+
+    else {
+        k.style.animation = 'kback 2s ease-in-out forwards'
+    }
+})
+
+/* line_title 클래스애니메이션 */ 
+let line_title = document.querySelector('.line_title')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 6713) {
+        line_title.style.animation = 'k 2s ease-in-out forwards' 
+    }
+
+    else {
+        line_title.style.animation = 'kback 2s ease-in-out forwards'
+    }
+})
+
+/* s_line 클래스애니메이션 */ 
+let s_line = document.querySelector('.s_line')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 6713) {
+        s_line.style.animation = 's_line 2s ease-in-out forwards' 
+    }
+
+    else {
+        s_line.style.animation = 's_lineback 2s ease-in-out forwards'
+    }
+})
+
+/* logo_background10 클래스애니메이션 */ 
+let logo_background10 = document.querySelector('.logo_background10')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 6713) {
+        logo_background10.style.animation = 's_line 2s ease-in-out forwards' 
+    }
+
+    else {
+        logo_background10.style.animation = 's_lineback 2s ease-in-out forwards'
+    }
+})
+
+/* logo10 클래스애니메이션 */ 
+let logo10 = document.querySelector('.logo10')
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    console.log("scrollY", value);
+
+    if (value > 6713) {
+        logo10.style.animation = 'logo10 2s ease-in-out forwards' 
+    }
+
+    else {
+        logo10.style.animation = 'logo10back 2s ease-in-out forwards'
     }
 })
