2aa59bac9ff71a7d084401cc21ad45bcc6ee2cc3
DeokWon Kim <khjs7878@naver.com>
2024-02-06 19:15:07 +0900

    2024/02/06반응형 수정3

--- scroll.js
+++ scroll.js
@@ -2,7 +2,7 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     document.addEventListener('DOMContentLoaded', function () {
       // 가로 스크롤바를 감추는 스타일을 동적으로 추가
-      var style = document.createElement('style');
+      var style = document.createElement('mobile');
       style.innerHTML = 'body { overflow-x: hidden; }';
       document.head.appendChild(style);
     });
